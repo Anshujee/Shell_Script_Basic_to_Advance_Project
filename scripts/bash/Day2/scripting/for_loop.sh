6745416820e9cd9a9c1502fc680f1e3a054d2b46
Anshu <jeeanshu24@gmail.com>
2025-12-06 11:16:23 +0530

    Commit 5

--- scripts/bash/Day2/scripting/for_loop.sh
+++ scripts/bash/Day2/scripting/for_loop.sh
@@ -35,3 +35,46 @@ echo " ==== Number Range from 10 to 1 with step of -1 ===="
 for i in $(seq 10 -2 1); do
     echo " Number: $i"
 done
+echo ""
+# Loop through C -style syntax
+echo " ==== C-Style For Loop from 1 to 5 ===="
+for (( i=1; i<=10; i++ )); do
+    echo " Number: $i"
+done
+echo ""
+# Loop through an array
+echo " ==== Loop through an Array of Fruits ===="
+fruits=("Apple" "Banana" "Mango" "Grapes" "Oranges")
+for fruit in "${fruits[@]}"; do
+    echo " Fruit: $fruit"
+done
+echo ""
+# Loop through files in a directory
+echo " ==== Loop through Files in /etc Directory ===="
+for file in /etc/*; do
+    echo " File: $(basename "$file")"
+done
+echo ""
+# Nested for loop example
+echo " ==== Nested For Loop: Multiplication Table (1 to 5) ===="
+for i in {0..5}; do
+    for j in {0..5}; do
+        product=$((i * j))
+      echo -n "$product" 
+    done
+    echo ""
+done
+echo ""
+#for MAC book
+#!/bin/bash
+
+echo " ==== Nested For Loop: Multiplication Table (0 to 5) ===="
+
+for i in $(seq 0 5); do
+    for j in $(seq 0 10); do
+        product=$((i * j))
+        echo -n "$product "
+    done
+    echo ""
+done
+echo ""
